e353efbe32038f000807cdac20d2d7f5d22173e7
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-06-24 20:45:23 +0200

    starts js form validation

--- age-calculator-app-main/script.js
+++ age-calculator-app-main/script.js
@@ -1,7 +1,4 @@
-
-document.getElementById("dateForm").addEventListener("submit", formSubmit);
-
-function formSubmit(e) {
+document.getElementById("dateForm").addEventListener("submit", e => {
     e.preventDefault();
     const day = document.getElementById("day").value;
     const month = document.getElementById("month").value;
@@ -11,6 +8,10 @@ function formSubmit(e) {
     const age = today - birthDate;
     const monthsFromAge = age / 2629800000;
 
+    if (day > daysInMonth(month, year)) {
+        console.log("no valid date")
+    };
+
     const years = Math.floor(monthsFromAge / 12);
     const months = Math.floor(monthsFromAge % 12);
     const days = Math.floor((((monthsFromAge / 12) / 12 * 31) + (years / 4) ) / 24);
@@ -18,6 +19,42 @@ function formSubmit(e) {
     document.getElementById("years-result").innerText = Math.floor(years);
     document.getElementById("months-result").innerText = Math.floor(months);
     document.getElementById("days-result").innerText = Math.floor(days);
-}
+})
 
-// 9523.697238865741
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+day.addEventListener("blur", e => {
+    if (day.value == "" || day.value == NaN || day.value == "") {    
+    console.log("non hai messo il giorno")
+    } else if ((day.value <= 0 || day.value > 31) && day.value !== NaN) {
+        console.log("nu va bene, numero grande")
+    } else {
+        return;
+    }
+});
+(month.value <= 0 || month.value > 12)
+month.addEventListener("blur", e => {
+    if (month.value == "" || month.value == NaN || month.value == "") {
+        console.log("nu hai messo mese")
+    } else if (day.value > daysInMonth(month.value, year.value)) {
+        console.log("no valid date")
+    } else {
+        return
+    }
+});
+
+year.addEventListener("blur", e => {
+    if (year.value == "" || year.value == NaN || year.value == "") {    
+    console.log("non hai messo l'anno")
+    } else if (year.value <= 1930 || year.value > 2023) {
+        console.log("nu va bene, anno grande")
+    } else {
+        return
+    }
+});
+
+function daysInMonth(m, y){
+    return m===2?y&3||!(y%25)&&y&15?28:29:30+(m+(m>>3)&1);
+};
