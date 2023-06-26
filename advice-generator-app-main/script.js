80a2d13c0cabef9823e6b3602529f0079805b629
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-06-26 12:36:24 +0200

    adds button logic

--- advice-generator-app-main/script.js
+++ advice-generator-app-main/script.js
@@ -1,15 +1,29 @@
-async function getAdvice() {
-    const response = await fetch("https://api.adviceslip.com/advice");
+async function getAdvice() { 
+    let response = await fetch("https://api.adviceslip.com/advice");
     const data = await response.json();
     const {id, advice} = data.slip; 
+
+    if (advice.length > 70) {
+        response = await fetch("https://api.adviceslip.com/advice");
+    };
+
+    if (document.getElementById("advice-text").innerHTML !== "") {
+        document.getElementById("advice-text").innerHTML = "";
+    }
+    
+    if (document.getElementById("advice-id").innerHTML !== "") {
+        document.getElementById("advice-id").innerHTML = "";
+    }
 
     const adviceIdParagraph = document.getElementById("advice-id");
     const idText = document.createTextNode(id);
     adviceIdParagraph.appendChild(idText);
 
     const adviceTextParagraph = document.getElementById("advice-text");
-    const adviceText = document.createTextNode(advice);
-    adviceIdParagraph.appendChild(adviceText);
+    const adviceText = document.createTextNode(`"${advice}"`);
+    adviceTextParagraph.appendChild(adviceText);
+
 }
 
 getAdvice();
+
