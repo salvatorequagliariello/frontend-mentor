fe046f8ea2f9cbe61c67c54d304c78575c57f822
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-06-30 10:14:28 +0200

    adds js dinamyc button  styling

--- time-tracking-dashboard-main/script.js
+++ time-tracking-dashboard-main/script.js
@@ -26,12 +26,49 @@ function changeTimes(timelapse) {
 
 day.addEventListener("click", e => {
     changeTimes("daily");
+    
+    day.style.color = "white";
+    day.style.opacity = "1";
+
+    week.style.color = "var(--neutral-color-blue-200)";
+    week.style.opacity = "0.6";
+
+    month.style.color = "var(--neutral-color-blue-200)";
+    month.style.opacity = "0.6";
 });
 
 week.addEventListener("click", e => {
     changeTimes("weekly");
+
+    day.style.color = "var(--neutral-color-blue-200)";
+    day.style.opacity = "0.6";
+
+    week.style.color = "white";
+    week.style.opacity = "1";
+
+    month.style.color = "var(--neutral-color-blue-200)";
+    month.style.opacity = "0.6";
 });
 
 month.addEventListener("click", e => {
     changeTimes("monthly");
+
+    day.style.color = "var(--neutral-color-blue-200)";
+    day.style.opacity = "0.6";
+
+    week.style.color = "var(--neutral-color-blue-200)";
+    week.style.opacity = "0.6";
+
+    month.style.color = "white";
+    month.style.opacity = "1";
 });
+
+changeTimes("weekly");
+day.style.color = "var(--neutral-color-blue-200)";
+day.style.opacity = "0.6";
+
+week.style.color = "white";
+week.style.opacity = "1";
+
+month.style.color = "var(--neutral-color-blue-200)";
+month.style.opacity = "0.6";
