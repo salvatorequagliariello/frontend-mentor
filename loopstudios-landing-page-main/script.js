06e94e515504215592afaecd15b23c6b97c8ea23
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-01 18:46:21 +0200

    starts mobile design

--- loopstudios-landing-page-main/script.js
+++ loopstudios-landing-page-main/script.js
@@ -1,13 +1,15 @@
 
-const header = document.getElementById("header");
+ const header = document.getElementById("header");
 
-window.addEventListener("scroll", () => {
+ window.addEventListener("scroll", () => {
 
-    if (window.scrollY > 120) {
-        header.style.backgroundColor = "black";
-        header.style.paddingTop = "1.5rem";
-    } else {
-        header.style.backgroundColor = "transparent";
-        header.style.paddingTop = "4rem";
-    };
+     if (window.scrollY > 120 && window.innerWidth > 800) {
+         header.style.backgroundColor = "black";
+         header.style.paddingTop = "1.5rem";
+     } else if (window.scrollY < 120 && window.innerWidth > 800) {
+         header.style.backgroundColor = "transparent";
+         header.style.paddingTop = "4rem";
+     } else {
+        return;
+     }
 })
