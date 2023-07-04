0d8f8a21cb826788c93b0beea705b27aa3a68b6c
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-04 19:09:24 +0200

    correct minor flaws on calculator screen

--- calculator-app-main/script.js
+++ calculator-app-main/script.js
@@ -1,10 +1,12 @@
 const screenText = document.getElementById("screenText");
 const buttons = document.getElementsByClassName("calculator-button");
+const dummy = document.getElementById("screenTextDummy");
 
 const operations = [];
 
 for (const button of buttons) {
     button.addEventListener("click", () => {
+        dummy.remove();
         if (button.value == "del") {
             screenText.innerHTML = screenText.innerHTML.substring(0, screenText.innerHTML.length - 1);
         } else if (!isNaN(button.value)) {
@@ -37,8 +39,8 @@ for (const button of buttons) {
 };
 
 function getResult(operations) {
-    
     let tempNum;
+
     
     for (let i = operations.length - 1; i => 0; i--) {
         if (!isNaN(operations[i])) {
@@ -56,9 +58,16 @@ function getResult(operations) {
             tempNum = +operations[i - 1] * tempNum;
             i--;
         } else {
+
+            if (!Number.isInteger(tempNum)) {
+                tempNum = +tempNum.toFixed(4);
+            };
             return tempNum;
         }
     }
-
+    
+    if (!Number.isInteger(tempNum)) {
+        tempNum = +tempNum.toFixed(4);
+    };
     return tempNum;
 } 
