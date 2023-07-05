7b0c661e57ca22419c06f0296d9ea144837dd5ea
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-05 16:31:15 +0200

    completes theme switcher function

--- calculator-app-main/themeChanger.js
+++ calculator-app-main/themeChanger.js
@@ -4,6 +4,10 @@ const body = document.body;
 const header = document.getElementById("calculatorHeader");
 const screen = document.getElementById("calculatorScreen");
 const textScreen = document.getElementsByClassName("calculator-screen__text");
+const calculatorKeypad = document.getElementsByClassName("calculator-buttons")[0];
+const buttons = document.getElementsByClassName("calculator-button");
+const twoButtons = [document.getElementById("buttonDel"), document.getElementById("buttonReset")];
+const equal = document.getElementById("buttonEqual");
 
 themeButton.addEventListener("change", () => {
     if (themeButton.value == "2") {
@@ -20,6 +24,22 @@ themeButton.addEventListener("change", () => {
             text.classList.remove("theme3-text-screen");
             text.classList.add("theme2-text-screen");
         };
+
+        calculatorKeypad.classList.remove("theme3-keypad-bg");
+        calculatorKeypad.classList.add("theme2-keypad-bg");
+
+        for (const button of buttons) {
+            button.classList.remove("theme3-btn-color");
+            button.classList.add("theme2-btn-color");
+        };
+
+        for (const btn of twoButtons) {
+            btn.classList.remove("theme3-reset-button");
+            btn.classList.add("theme2-reset-button");
+        };
+
+        equal.classList.remove("theme3-equal");
+        equal.classList.add("theme2-equal");
     } else if (themeButton.value == "3") {
         body.classList.remove("theme2-body");
         body.classList.add("theme3-body");
@@ -34,6 +54,22 @@ themeButton.addEventListener("change", () => {
             text.classList.remove("theme2-text-screen");
             text.classList.add("theme3-text-screen");
         };
+
+        calculatorKeypad.classList.remove("theme2-keypad-bg");
+        calculatorKeypad.classList.add("theme3-keypad-bg");
+
+        for (const button of buttons) {
+            button.classList.remove("theme2-btn-color");
+            button.classList.add("theme3-btn-color");
+        };
+
+        for (const btn of twoButtons) {
+            btn.classList.remove("theme2-reset-button");
+            btn.classList.add("theme3-reset-button");
+        };
+
+        equal.classList.remove("theme2-equal");
+        equal.classList.add("theme3-equal");
     } else {
         body.classList.remove("theme3-body");
         body.classList.remove("theme2-body");
@@ -48,5 +84,22 @@ themeButton.addEventListener("change", () => {
             text.classList.remove("theme3-text-screen");
             text.classList.remove("theme2-text-screen");
         };
+
+        calculatorKeypad.classList.remove("theme3-keypad-bg");
+        calculatorKeypad.classList.remove("theme2-keypad-bg");
+
+        for (const button of buttons) {
+            button.classList.remove("theme3-btn-color");
+            button.classList.remove("theme2-btn-color");
+        };
+
+
+        for (const btn of twoButtons) {
+            btn.classList.remove("theme3-reset-button");
+            btn.classList.remove("theme2-reset-button");
+        };
+
+        equal.classList.remove("theme3-equal");
+        equal.classList.remove("theme2-equal");
     }
 })
