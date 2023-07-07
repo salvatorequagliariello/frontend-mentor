e35842d5fb6f50e8a2a0ebca5612d5ed84648bc8
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-07-07 19:34:49 +0200

    adds carousel logic

--- ecommerce-product-page-main/script.js
+++ ecommerce-product-page-main/script.js
@@ -1,15 +1,69 @@
 
 const imgButtons = document.getElementsByClassName("image-button");
 const mainImg = document.getElementById("mainImg");
+const lightbox = document.getElementsByClassName("lightbox")[0];
+
+const lightMainImg = document.getElementById("lightMainImg");
+const carouselDivs = document.getElementsByClassName("lightbox-img-container");
+const prev = document.getElementById("lightPrev");
+const next = document.getElementById("lightNext");
+let preselected = -1;
+
+mainImg.addEventListener("click", () => {
+    lightbox.classList.add("flex");
+})
 
 for (const button of imgButtons) {
     button.addEventListener("click", () => {
         const otherButtons = Array.from(imgButtons);
         const index = otherButtons.indexOf(button);
+        preselected = index;
         otherButtons.splice(index, 1);
+        console.log(index);
+
+        getSelected(index);
 
         mainImg.src = `/images/image-product-${index + 1}.jpg`
         otherButtons.forEach(btn => btn.classList.remove("active-image"));
         button.classList.add("active-image");
     })
 }
+
+for (const div of carouselDivs) {
+    div.addEventListener("click", () => {
+        const allDivs = Array.from(carouselDivs);
+        const index = allDivs.indexOf(div);
+        allDivs.splice(index, 1);
+
+        
+        lightMainImg.src = `/images/image-product-${index + 1}.jpg`;
+        div.classList.add("orange-border");
+        div.querySelector(".overlay").classList.add("lightbox-white");
+
+        for (const btn of allDivs) {
+            btn.classList.remove("orange-border");
+            btn.querySelector(".overlay").classList.remove("lightbox-white");
+        }
+    })
+};
+
+next.addEventListener("click", () => {
+    let counter;
+
+    if (preselected == -1) {
+        counter == 0
+    } else {
+        counter = preselected;
+    }
+
+    
+});
+
+
+function getSelected(index) {
+    const allDivs = Array.from(carouselDivs);
+    lightMainImg.src = `/images/image-product-${index + 1}.jpg`;
+    allDivs[index].classList.add("orange-border");
+    allDivs[index].querySelector(".overlay").classList.add("lightbox-white");
+};
+
