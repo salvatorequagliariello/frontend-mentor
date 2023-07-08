2c088393435bd2c8b722e8f99d23a7929a3f9592
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-07-08 10:14:30 +0200

    add carousel logic

--- ecommerce-product-page-main/script.js
+++ ecommerce-product-page-main/script.js
@@ -18,8 +18,8 @@ for (const button of imgButtons) {
         const otherButtons = Array.from(imgButtons);
         const index = otherButtons.indexOf(button);
         preselected = index;
+        console.log("in", index, "pre", preselected);
         otherButtons.splice(index, 1);
-        console.log(index);
 
         getSelected(index);
 
@@ -33,9 +33,10 @@ for (const div of carouselDivs) {
     div.addEventListener("click", () => {
         const allDivs = Array.from(carouselDivs);
         const index = allDivs.indexOf(div);
+        preselected = index;
+        console.log("in", index, "pre", preselected);
         allDivs.splice(index, 1);
 
-        
         lightMainImg.src = `/images/image-product-${index + 1}.jpg`;
         div.classList.add("orange-border");
         div.querySelector(".overlay").classList.add("lightbox-white");
@@ -43,27 +44,75 @@ for (const div of carouselDivs) {
         for (const btn of allDivs) {
             btn.classList.remove("orange-border");
             btn.querySelector(".overlay").classList.remove("lightbox-white");
-        }
+        };
     })
 };
 
 next.addEventListener("click", () => {
-    let counter;
 
-    if (preselected == -1) {
-        counter == 0
-    } else {
-        counter = preselected;
+    if (preselected == -1) preselected = 0;
+
+    if (preselected == 3) {
+        preselected = 0;
+    } else if (preselected >= 0 && preselected <= 4) {
+        preselected += 1;
     }
 
-    
+    lightMainImg.src = `/images/image-product-${preselected + 1}.jpg`;
+
+    const allDivs = Array.from(carouselDivs);
+
+    allDivs[preselected].classList.add("orange-border");
+    allDivs[preselected].querySelector(".overlay").classList.add("lightbox-white");
+    allDivs.splice(preselected, 1);
+
+    for (const btn of allDivs) {
+        btn.classList.remove("orange-border");
+        btn.querySelector(".overlay").classList.remove("lightbox-white");
+    };
+});
+
+prev.addEventListener("click", () => {
+
+    if (preselected == -1) preselected = 0;
+
+    if (preselected === 0) {
+        preselected = 3;
+    } else {
+        preselected -= 1;
+    }
+
+    lightMainImg.src = `/images/image-product-${preselected + 1}.jpg`;
+
+    const allDivs = Array.from(carouselDivs);
+
+    allDivs[preselected].classList.add("orange-border");
+    allDivs[preselected].querySelector(".overlay").classList.add("lightbox-white");
+    allDivs.splice(preselected, 1);
+
+    for (const btn of allDivs) {
+        btn.classList.remove("orange-border");
+        btn.querySelector(".overlay").classList.remove("lightbox-white");
+    };
 });
 
 
-function getSelected(index) {
+
+function getSelected(index = -1) {
+    if (index === -1) index = 0;
+
     const allDivs = Array.from(carouselDivs);
     lightMainImg.src = `/images/image-product-${index + 1}.jpg`;
+
     allDivs[index].classList.add("orange-border");
     allDivs[index].querySelector(".overlay").classList.add("lightbox-white");
+    
+    allDivs.splice(index, 1);
+    for (const btn of allDivs) {
+        btn.classList.remove("orange-border");
+        btn.querySelector(".overlay").classList.remove("lightbox-white");
+    }
 };
+
+getSelected();
 
