e24695c3e8c8d3fb057616571608b165b12880fa
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-07-09 13:24:05 +0200

    add cart style

--- ecommerce-product-page-main/cart.js
+++ ecommerce-product-page-main/cart.js
@@ -2,11 +2,29 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const cartCounterDiv = document.getElementById("cartCounter");
 const addCart = document.getElementById("addCart");
+const cartIcon = document.getElementById("cartIcon");
+const cartContainer = document.getElementsByClassName("cart-container")[0];
+
 let buyCounter = 0;
 let cartCounter = 0;
 
+cartIcon.addEventListener("click", () => {
+    if (cartContainer.classList.contains("cart-open")) {
+        cartContainer.classList.remove("cart-open");
+    } else {
+        cartContainer.classList.add("cart-open");
+    };
+})
+
 addCart.addEventListener("click", () => {
-    buyCounter = cartCounter;
+    if (cartCounter >= 1) {
+        buyCounter = cartContainer;
+        document.getElementsByClassName("cart-empty")[0].style.display = "none";
+        document.getElementsByClassName("cart-full")[0].style.display = "block";
+    } else {
+        document.getElementsByClassName("cart-full")[0].style.display = "none";
+        document.getElementsByClassName("cart-empty")[0].style.display = "flex";
+    }
 })
 
 plus.addEventListener("click", () => {
@@ -15,7 +33,6 @@ plus.addEventListener("click", () => {
 })
 
 minus.addEventListener("click", () => {
-
     if (cartCounter >= 1) {
         cartCounter -= 1;
     } else {
