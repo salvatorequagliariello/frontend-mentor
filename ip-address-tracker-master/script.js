bafbbe5c004f81302ed687c9f7ddedd43ea8bd4c
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-07-12 15:56:50 +0200

    add marker logic

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -5,6 +5,12 @@ const ispOut = document.getElementById("ispOut");
 
 const inputId = document.getElementById("inputId");
 const button = document.getElementById("searchButton");
+const mapDiv = document.getElementById("map");
+
+const markerIcon = L.icon({
+    iconUrl: `/images/icon-location.svg`,
+    iconAnchor: [22, -20],
+}) 
 
 const getIp = async (ip) => {
     const response = await fetch(`http://ip-api.com/json/${ip}`);
@@ -20,6 +26,17 @@ const getIp = async (ip) => {
     locationOut.innerText = location;
     timezoneOut.innerText = `UTC ${timezoneString}`;
     ispOut.innerText = isp;
+
+    const map = L.map('map', {
+        center: [data.lat, data.lon],
+        zoom: 17,
+    });
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    }).addTo(map);
+    const marker = L.marker(map.getCenter(), {icon: markerIcon}).addTo(map);
+    marker.setLatLng(map.getCenter());
+    map.zoomControl = false;
 }
 
 button.addEventListener("click", e => {
