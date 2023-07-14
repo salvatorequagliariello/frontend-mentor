d84a1238ead9ff9bee4690d61c018d4e22c69c06
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-07-14 18:45:04 +0200

    add new todo logic

--- todo-app-main/script.js
+++ todo-app-main/script.js
@@ -1,25 +1,42 @@
 const inputTodo = document.getElementById("inputTodo");
 const addButton = document.getElementById("addButton");
-const todoList = document.getElementsByClassName("todo-list")[0];
+
+const todoAllList = document.getElementsByClassName("todo-list")[0];
 const todoCountDiv = document.getElementById("todo-count");
+
 const todoAllArray = [];
 const todoCompletedArray = [];
 
+const buttonsInList = todoAllList.querySelectorAll(".add-button");
+
+// ADD TODO TO ALL LIST AND ARRAY
 const createTodo = (text) => {
     const todoContainer = document.getElementsByClassName("todo-container")[0].cloneNode(true);
     const todoText = todoContainer.querySelector(".todo-text");
     todoText.innerText = text;
+    const closeButton = todoContainer.querySelector(".todo-close");
+    const checkButton = todoContainer.querySelector(".add-button");
 
-    todoList.appendChild(todoContainer);
+    closeButton.addEventListener("click", () => {
+        const index = todoAllArray.indexOf(todoAllArray)
+    })
+
     todoAllArray.push({
         completed: false,
-        element: todoContainer
+        element: todoContainer,
+        todoText: text, 
     });
 
+    for (const todo of todoAllArray) {
+        todoAllList.appendChild(todo.element);
+    }
+
     todoCountDiv.innerText = todoCompletedArray > 0 ? todoAllArray.length - todoCompletedArray.length : todoAllArray.length;
+
 };
+
+
 
 addButton.addEventListener("click",() => {
     createTodo(inputTodo.value);
-
 })
